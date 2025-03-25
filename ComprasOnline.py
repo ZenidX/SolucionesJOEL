a9f68638f77d3ea090223e4f98d39a2b52e1456a
ZenidX <zenid77@gmail.com>
2025-03-25 08:19:19 +0100

    Asegurar ComprasOnline.py

--- ComprasOnline.py
+++ ComprasOnline.py
@@ -10,33 +10,34 @@ for i in range(n_casos):
         casos[-1]["tiendas"][-1].update({"n_articulos":int(input())})
         casos[-1]["tiendas"][-1].update({"articulos":list()})
         for k in range(casos[-1]["tiendas"][-1]["n_articulos"]):
-            casos[-1]["tiendas"][-1]["articulos"].append()
-    print(casos[-1])
+            casos[-1]["tiendas"][-1]["articulos"].append(input().split(" "))
     casos[-1].update({"n_articulos":int(input())})
     casos[-1].update({"compras":list()})
     for j in range(casos[-1]["n_articulos"]):
-        casos[-1]["compras"].append(input().split())
-precio=list()
-cantid=list()
+        casos[-1]["compras"].append(input().split(" "))
+        
 #Calculus
+precios=list()
+cantidades=list()
 def calculus(caso):
     suma=0
     for compra in caso["compras"]:
         precios=list()
         cantidades=list()
         for tienda in caso["tiendas"]:
-            if tienda[0]==compra[0]:
-                precios.append(int(j[1]))
-                cantidades.append(int(j[2]))
-        for i in range(int(i[1])):
-            precio_minimo=precios[0]
-            for precio in precios:
-                if cantidades[precios.index[precio]]>0:
-                    precio_minimo=min(i,precio_minimo)
-            suma+=precios[precios.index[precio]]
-            cantidades[precios.index[precio]]-=1
+            for articulo in tienda["articulos"]:
+                if articulo[0]==compra[0]:
+                    precios.append(int(articulo[1]))
+                    cantidades.append(int(articulo[2]))
+        for i in range(int(compra[1])):
+            precio_minimo=100
+            for j in range(len(precios)):
+                if cantidades[j]>0:
+                    precio_minimo=min(precios[j],precio_minimo)
+            suma+=precio_minimo
+            cantidades[precios.index(precio_minimo)]-=1
     return suma
 
 #Output
-for i in casos:
-    calculus(caso)
+for caso in casos:
+    print(calculus(caso))
