e61184ba90fd22d872f4f5339c0942c4856a6e15
ZenidX <zenid77@gmail.com>
2025-03-31 01:08:14 +0200

    Cosas

--- MapaProcessosQualitat.py
+++ MapaProcessosQualitat.py
@@ -7,15 +7,17 @@ for i in range(n_casos):
     for j in range(n_tareas):
         casos[i].append(input().split("-"))
         casos[i][j][1]=casos[i][j][1].split(",")
-for i in casos:
-    for j in i:
-        print(*j)
+
+
 #Calculus
 def calculus(caso):
-    result=0
+    orden=dict()
+    for i in caso:
+        orden.add({i[0]:len(i[1])})
+    for i in range(len(orden)):
+        
     return result
 
 #Output
 for i in casos:
     print(*calculus(i))
-
