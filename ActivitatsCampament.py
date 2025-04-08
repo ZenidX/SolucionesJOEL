afc89839a9dcbdbd2c41c8edaa6bb950b2654fcd
ZenidX <111122142+ZenidX@users.noreply.github.com>
2025-04-08 19:32:34 +0200

    Update ActivitatsCampament.py

--- ActivitatsCampament.py
+++ ActivitatsCampament.py
@@ -7,25 +7,36 @@ for i in range(n_casos):
     "acti":int(input()),
     "capa":[int(i) for i in input().split()]
   }
+  casos.append(caso)
 
 #Calculus
 def calculus(caso):
     j=0
     for i in range(caso["acti"]):
         nens_acti=[]
-        while caso["capa"]!=0 or j==len(caso["nens"]):
+        while caso["capa"][i]>=0 and j<len(caso["nens"]):
             nens_acti.append(caso["nens"][j])
             caso["capa"][i]=caso["capa"][i]-1
             j=j+1
-        print(f"Activitat {i+1}: {nens_acti}")
+        print(f"Activitat {i+1}: ",end="")
+        print("[",end="")
+        for i in range(len(nens_acti)-1):
+            print(f"{nens_acti[i]}, ", end="")
+        if nens_acti!=[]:
+            print(f"{nens_acti[-1]}]")
+        else: 
+
     if j==len(caso["nens"]):
         print("Tots els nenes han estat assignats")
     else:
-        print("Nens sobrants: ","")
-        while j!=len(caso["nens"]):
-            print(caso["nens"][j],"")
+        print("Nens sobrants: ",end="")
+        while j<len(caso["nens"]):
+            print(caso["nens"][j],end="")
+            j=j+1
         print("")
+    print("")
+    return
 
 #Output
 for caso in casos:
-  calculus(caso)
+    calculus(caso)
